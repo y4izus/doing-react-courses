a4ea3df5cfd3cc922897e7d9e97c0487ede38de9
y4izus <yaiza.garcia.mm@gmail.com>
2018-06-07 14:18:40 +0200

    Recognize spread operator

--- 3.-ecommerce_example/webpack.config.js
+++ 3.-ecommerce_example/webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
          exclude: /node_modules/,
          loader: 'babel-loader',
          options: {
-            presets: ['es2015', 'react'],
+            presets: ['es2015', 'react', 'stage-0'],
             plugins: ['react-hot-loader/babel'],
          }
       }]
